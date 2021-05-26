ab32088355f5597087876c0b5de7ed40705ea516
SofranSebastian <sofransebastian@yahoo.com>
2021-05-26 19:44:09 +0300

    TasksViewerViewModel.kt: now the tasks list is managed by the VM

--- app/src/main/java/com/example/taskkeeper/tasksviewer/TasksViewerViewModel.kt
+++ app/src/main/java/com/example/taskkeeper/tasksviewer/TasksViewerViewModel.kt
@@ -5,12 +5,26 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class TasksViewerViewModel(item: List<TaskItem>) : ViewModel() {
+class TasksViewerViewModel() : ViewModel() {
+
+    private val _tasksList = MutableLiveData<List<TaskItem>>(listOf(
+            TaskItem(1,"Task1","Random subtitle."),
+            TaskItem(2,"Task2","Random subtitle."),
+            TaskItem(3,"Task3","Random subtitle."),
+            TaskItem(1,"Task4","Random subtitle."),
+            TaskItem(2,"Task5","Random subtitle."),
+            TaskItem(0,"Task6","Random subtitle."),
+            TaskItem(1,"Task7","Random subtitle."),
+            TaskItem(1,"Task8","Random subtitle."),
+            TaskItem(3,"Task9","Random subtitle."),
+            TaskItem(2,"Task10","Random subtitle.")
+    ))
+    val tasksList : LiveData<List<TaskItem>>
+        get() = _tasksList
 
     private val _seeMoreClicked = MutableLiveData<Boolean>(false)
     val seeMoreClicked : LiveData<Boolean>
         get() = _seeMoreClicked
-
 
     fun onClick(){
         _seeMoreClicked.value?.let{
