d044a14b2973a321b490909e5df75e0b3629ea55
SofranSebastian <sofransebastian@yahoo.com>
2021-06-16 20:06:46 +0300

    TaskAdapter.kt: changes due to the database integration

--- app/src/main/java/com/example/taskkeeper/tasksviewer/TaskAdapter.kt
+++ app/src/main/java/com/example/taskkeeper/tasksviewer/TaskAdapter.kt
@@ -3,9 +3,11 @@ package com.example.taskkeeper.tasksviewer
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.taskkeeper.database.TaskItem
 import com.example.taskkeeper.databinding.ItemTaskLayoutBinding
 
-class TaskAdapter(private val tasksList: List<TaskItem>, val clickListener: TaskListener) : RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
+class TaskAdapter(private val tasksList: MutableList<TaskItem>, val clickListener: TaskListener) :
+    RecyclerView.Adapter<TaskAdapter.ViewHolder>() {
 
     //class for the view holder which takes as parameter a layout
     class ViewHolder(val binding: ItemTaskLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
@@ -18,7 +20,8 @@ class TaskAdapter(private val tasksList: List<TaskItem>, val clickListener: Task
 
     //return the View Holder created above with an inflater
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemTaskLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemTaskLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ViewHolder(binding)
     }
@@ -28,8 +31,14 @@ class TaskAdapter(private val tasksList: List<TaskItem>, val clickListener: Task
     }
 
     override fun getItemCount() = tasksList.size
+
+    fun setList(list: List<TaskItem>) {
+        tasksList.clear()
+        tasksList.addAll(list)
+        notifyDataSetChanged()
+    }
 }
 
-class TaskListener(val clickListener: (title: String) -> Unit) {
-    fun onClick(item: TaskItem) = clickListener(item.title)
+class TaskListener(val clickListener: (item: TaskItem) -> Unit) {
+    fun onClick(item: TaskItem) = clickListener(item)
 }
