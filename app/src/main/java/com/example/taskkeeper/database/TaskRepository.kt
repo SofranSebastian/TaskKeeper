0b417ed9657d1ffe61c35dcd15056bc973917383
SofranSebastian <sofransebastian@yahoo.com>
2021-06-16 20:08:47 +0300

    TaskRepository.kt: added new functions for database operations

--- app/src/main/java/com/example/taskkeeper/database/TaskRepository.kt
+++ app/src/main/java/com/example/taskkeeper/database/TaskRepository.kt
@@ -5,7 +5,19 @@ import androidx.lifecycle.LiveData
 class TaskRepository(private val taskDao: TaskDao) {
     val getAllTasks: LiveData<List<Task>> = taskDao.getAllTasks()
 
-    suspend fun addTask(task: Task){
+    suspend fun addTask(task: Task) {
         taskDao.addTask(task)
+    }
+
+    suspend fun updateTask(task: Task) {
+        taskDao.updateTask(task)
+    }
+
+    suspend fun deleteTask(task: Task) {
+        taskDao.deleteTask(task)
+    }
+
+    suspend fun deleteAllTasks() {
+        taskDao.deleteAllTasks()
     }
 }
