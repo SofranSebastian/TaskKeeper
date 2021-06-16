b45cb5fd672dca47b93539def16fbed27b51ecbd
SofranSebastian <sofransebastian@yahoo.com>
2021-06-16 20:06:19 +0300

    Task.kt: entity for the database

--- app/src/main/java/com/example/taskkeeper/database/Task.kt
+++ app/src/main/java/com/example/taskkeeper/database/Task.kt
@@ -5,11 +5,10 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "task_table")
 data class Task(
-
     //Room library will auto generate the PK
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val title: String,
     val description: String,
-    val priority: Int,
+    val priority: String,
 )
