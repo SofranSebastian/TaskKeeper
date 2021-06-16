0e89ef9bfb834b470308f2099b0b7326ccb3dc5d
SofranSebastian <sofransebastian@yahoo.com>
2021-06-16 20:10:46 +0300

    TasksViewerViewModel.kt: changes to the VM of the Tasks Viewer due to database changes

--- app/src/main/java/com/example/taskkeeper/tasksviewer/TasksViewerViewModel.kt
+++ app/src/main/java/com/example/taskkeeper/tasksviewer/TasksViewerViewModel.kt
@@ -4,28 +4,38 @@ import android.app.Application
 import androidx.lifecycle.*
 import com.example.taskkeeper.database.Task
 import com.example.taskkeeper.database.TaskDatabase
+import com.example.taskkeeper.database.TaskItem
 import com.example.taskkeeper.database.TaskRepository
+import com.example.taskkeeper.utils.toTaskItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class TasksViewerViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val repository : TaskRepository
-
-    private val _tasksList : LiveData<List<Task>>
-    val tasksList: LiveData<List<Task>>
-        get() = _tasksList
+    private val repository: TaskRepository
 
     //first executed when the TasksViewerViewModel is called
     init {
         val taskDao = TaskDatabase.getDatabase(application).userDao()
         repository = TaskRepository(taskDao)
-        _tasksList = repository.getAllTasks
+    }
+
+    private val tasksList: LiveData<List<Task>>
+        get() = repository.getAllTasks
+
+    val tasksListMapped: LiveData<List<TaskItem>> = Transformations.map(tasksList) { list ->
+        list.map { it.toTaskItem() }
     }
 
     fun addTask(task: Task) {
         viewModelScope.launch(Dispatchers.IO) {
             repository.addTask(task)
+        }
+    }
+
+    fun deleteAllTasks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.deleteAllTasks()
         }
     }
 
@@ -39,12 +49,12 @@ class TasksViewerViewModel(application: Application) : AndroidViewModel(applicat
         }
     }
 
-    private val _navigateToTaskDetail = MutableLiveData<String>()
+    private val _navigateToTaskDetail = MutableLiveData<TaskItem>()
     val navigateToTaskDetail
         get() = _navigateToTaskDetail
 
-    fun onTaskClicked(title: String) {
-        _navigateToTaskDetail.value = title
+    fun onTaskClicked(task: TaskItem) {
+        _navigateToTaskDetail.value = task
     }
 
     fun onTaskDetailNavigated() {
