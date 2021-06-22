f881aa31f7a92d39623cb3334ca7634ae5b956e5
SofranSebastian <sofransebastian@yahoo.com>
2021-06-22 19:26:29 +0300

    HolidaysViewerViewModel.kt: created the VM for the holidays

---
+++ app/src/main/java/com/example/taskkeeper/holidays/HolidaysViewerViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.taskkeeper.holidays
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.taskkeeper.holidays.model.HolidayObject
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class HolidaysViewerViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository: HolidaysRepository = HolidaysRepository()
+
+    val holidaysList: MutableLiveData<Response<List<HolidayObject>>> = MutableLiveData()
+
+    fun getHolidays(year: Int, code: String) {
+        viewModelScope.launch {
+            val response = repository.getHolidays(year, code)
+            holidaysList.value = response
+        }
+    }
+
+}
