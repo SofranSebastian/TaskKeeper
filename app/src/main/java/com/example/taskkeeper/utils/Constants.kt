f3c02f24d49cefb2de893ebca5cc6138dbd18cf2
SofranSebastian <sofransebastian@yahoo.com>
2021-06-22 19:17:57 +0300

    Constants.kt: created a class for holding constant values

---
+++ app/src/main/java/com/example/taskkeeper/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.taskkeeper.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://date.nager.at"
+        const val MAXIMUM_YEAR = 2050
+        const val MINIMUM_YEAR = 1950
+        const val CURRENT_YEAR = 2021
+    }
+}
