6a15f932b1909415a644ec0f77eb63ebeb76e040
SofranSebastian <sofransebastian@yahoo.com>
2021-06-22 19:17:04 +0300

    Api.kt: created the API interface with the getHolidays method

---
+++ app/src/main/java/com/example/taskkeeper/api/Api.kt
@@ -0,0 +1,16 @@
+package com.example.taskkeeper.api
+
+import com.example.taskkeeper.holidays.model.HolidayObject
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface Api {
+
+    @GET("/api/v3/PublicHolidays/{yearChosen}/{countryCodeChosen}")
+    suspend fun getHolidays(
+        @Path("yearChosen") year: Int,
+        @Path("countryCodeChosen") code: String
+    ): Response<List<HolidayObject>>
+
+}
