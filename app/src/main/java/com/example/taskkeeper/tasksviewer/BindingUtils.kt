64bee72ac174f59904edbfaec79432fbcef8665e
SofranSebastian <sofransebastian@yahoo.com>
2021-06-22 19:09:37 +0300

    BindingUtils.kt: deleted a method which is no longer used

--- app/src/main/java/com/example/taskkeeper/tasksviewer/BindingUtils.kt
+++ app/src/main/java/com/example/taskkeeper/tasksviewer/BindingUtils.kt
@@ -1,34 +1,20 @@
 package com.example.taskkeeper.tasksviewer
 
 import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.databinding.BindingAdapter
 import com.example.taskkeeper.database.TaskItem
 
-
 @BindingAdapter("titleText")
 fun TextView.setTitleText(item: TaskItem) {
-    item?.let {
-        text = item.title?.toUpperCase()
-    }
+    text = item.title?.toUpperCase()
 }
 
 @BindingAdapter("subtitleText")
 fun TextView.setSubtitleText(item: TaskItem) {
-    item?.let {
-        text = item.description
-    }
-}
-
-@BindingAdapter("cardViewBackground")
-fun CardView.setBackgroundCard(item: TaskItem) {
-    item?.let {
-    }
+    text = item.description
 }
 
 @BindingAdapter("priorityText")
 fun TextView.setPriorityText(item: TaskItem) {
-    item?.let {
-        text = item.priority
-    }
+    text = item.priority
 }
