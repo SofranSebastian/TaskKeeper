b53fa5a977eae0b37edd1082397189c95ac2794d
SofranSebastian <sofransebastian@yahoo.com>
2021-07-01 18:50:09 +0300

    RepositoryModule.kt: created a module for repositories

---
+++ app/src/main/java/com/example/taskkeeper/dagger/modules/RepositoryModule.kt
@@ -0,0 +1,25 @@
+package com.example.taskkeeper.dagger.modules
+
+import android.content.Context
+import com.example.taskkeeper.repository.HolidaysRepository
+import com.example.taskkeeper.repository.TaskRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun provideHolidaysRepository() = HolidaysRepository()
+
+    @Provides
+    @Singleton
+    fun provideTasksRepository(@ApplicationContext context: Context) =
+        TaskRepository(DatabaseModule.provideTaskDao(context))
+}
