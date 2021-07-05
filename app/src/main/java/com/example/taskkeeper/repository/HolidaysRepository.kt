fcc670af2b6e879cc6577113782deb3c22dafd2f
SofranSebastian <sofransebastian@yahoo.com>
2021-07-05 18:33:20 +0300

    HolidaysRepository.kt: the retrofit instance is now injected in the repository contructor

--- app/src/main/java/com/example/taskkeeper/repository/HolidaysRepository.kt
+++ app/src/main/java/com/example/taskkeeper/repository/HolidaysRepository.kt
@@ -3,11 +3,14 @@ package com.example.taskkeeper.repository
 import com.example.taskkeeper.api.RetrofitInstance
 import com.example.taskkeeper.ui.holidays.model.HolidayObject
 import retrofit2.Response
+import javax.inject.Inject
 
-class HolidaysRepository {
+class HolidaysRepository @Inject constructor(
+    private val retrofitInstance: RetrofitInstance
+){
 
     suspend fun getHolidays(year: Int, code: String): Response<List<HolidayObject>> {
-        return RetrofitInstance.api.getHolidays(year, code)
+        return retrofitInstance.api.getHolidays(year, code)
     }
 
 }
